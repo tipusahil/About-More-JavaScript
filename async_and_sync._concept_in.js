d2187347f2973f550f5eef75de3562340503bc06
tipusahil <tipusahil.ctg@gmail.com>
2025-02-28 01:37:39 +0600

    3rd commit of call stack concept fundamental in js

--- async_and_sync._concept_in.js
+++ async_and_sync._concept_in.js
@@ -2,3 +2,15 @@
 
 1.normal function secuentially cole, kintu (fetch function & setTimeOut,setinterval secuentially colena.)
 */
+
+console.log(1);
+console.log(2);
+// console.log(3);
+func_secuenctiallyCole()
+console.log(4);
+console.log(5);
+
+function func_secuenctiallyCole()  {
+    console.log(3)
+    
+}
