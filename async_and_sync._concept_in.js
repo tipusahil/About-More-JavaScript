54bdb31ca1ae10df4d2ab80de52ee3669ca68c32
tipusahil <tipusahil.ctg@gmail.com>
2025-02-28 02:27:35 +0600

    settimeout & setinterval concept noted by me

--- async_and_sync._concept_in.js
+++ async_and_sync._concept_in.js
@@ -1,5 +1,6 @@
 /* 
-
+#1. jei function gulo js er single threated niom follow kore serial ways  cole segulo k bola hoi (synchronous)(ortat serially single threaded niom ta jara follow kore).jmn: function, ba kuno funcitoncall kora hole ,kuno varialbe console kora hole egul oserial maintain kore  output dibe, kintu (asynchronous) jegulo ase segulo serial maintain kore output dibena.
+#2. ar jegulo js er single threaded niom follow na kore nijder smy mot cole  segulo k bola hoi (asynchronous) ( ortat jegulo js er single threaded follow na kore nijeder moto kore cole) . jmn: (fetch),(settimeout),(setinterval), egulo (asynchronous ortat serial/single_threated follow korena,) & egulo serial maintain kroena, nijder icce moto age pore jkn tader modde set kore dewa smy hoi tkni output dibe, & (fetch) output dibe ,tkni jkn se (api) theke promise kora kuno data pabe tkn
 1.normal function secuentially cole, kintu (fetch function & setTimeOut,setinterval secuentially colena.)
 // function secuentially cole (ortat jkn call line dhroe 5jon thakle sei function joto number serial e ase sei serail onujai se jabe, but 
 // 1.(fetch) & 
@@ -7,21 +8,22 @@
 // 3.(setInterval) ei 3ta serial ways colena, tader nijeder icce moto cole) , ortat serially cole ,
 */
 
-console.log(1);
+console.log(1);// (synchronous/single_threated) niom follow kore serial wys output dibe.
 console.log(2);
 // console.log(3);
+
 func_secuenctiallyCole()// function secuentially cole (ortat jkn call line dhroe 5jon thakle sei function joto number serial e ase sei serail onujai se jabe, but 
 // 1.(fetch) & 
 // 2.(settimeout) & 
 // 3.(setInterval) ei 3ta serial ways colena, tader nijeder icce moto cole) , ortat serially cole ,
-console.log(4);
+console.log(4);// (synchronous/single_threated) niom follow kore serial wys output dibe.
 
 setTimeout(() =>{ console.log('(setTimeout) nijer icce moto 4 second pore output dekabe,') } , 4000);
 
-setInterval(() =>{ console.log('(setInterval) nijer icce moto take jei time set kore dewa hoise sei time por por bar bar colte takbe.') } , 2000);//ekane (setinterval) er kaj ta tar modde set kora dewa smy por por coltei thakbe, continously so 2tai off korar (settimeout & setinterval) off korar ekta way ase seta holo 
+setInterval(() =>{ console.log('(setInterval) nijer icce moto take jei time set kore dewa hoise sei time por por bar bar colte takbe.') } , 2000);//eta 2 second proe ouptut dibe karon eta ( asynchronous/single_threated follow kroena )ekane (setinterval) 2nd peramitar hisebe set kora dewa smy por por coltei thakbe, continously so 2tai off korar (settimeout & setinterval) off korar ekta way ase seta holo 
 
 
-console.log(5);
+console.log(5);// (synchronous/single_threated) niom follow kore serial wys output dibe.
 
 function func_secuenctiallyCole()  {
     console.log(3)
--- js_run_by_nature_sngle_threated.js
+++ js_run_by_nature_sngle_threated.js
@@ -3,5 +3,7 @@
 1.(fetch) nijer promise er back pawar jonnowait kore, kintu js coltei thakbei sei aste late koruk ar jai koruk
 2.(settimeout) etaw nier modde set kore dewa smy follow kore cole.
 3.(setinterval) etaw nijer modde set kore dewa smy follow kore cole,kintu eta or modde set kore dewa smy por por loop er moto infinity vabe coltei thakbe, but off kora jai seta.
+
+
  
 */
