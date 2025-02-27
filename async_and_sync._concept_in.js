0ea79efa3ec0889ebab6f008cf176f711cf2090e
tipusahil <tipusahil.ctg@gmail.com>
2025-02-28 01:55:13 +0600

    3rd commit of call stack concept fundamental in js

--- async_and_sync._concept_in.js
+++ async_and_sync._concept_in.js
@@ -6,11 +6,11 @@
 console.log(1);
 console.log(2);
 // console.log(3);
-func_secuenctiallyCole()
+func_secuenctiallyCole()// function secuentially cole (ortat jkn call kora hoise tkni cole.) , ortat serially cole ,
 console.log(4);
 console.log(5);
 
 function func_secuenctiallyCole()  {
     console.log(3)
-    
+
 }
