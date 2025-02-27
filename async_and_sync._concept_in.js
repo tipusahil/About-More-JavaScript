754ff4ee9c56514bce3c99d58f93eb64ebdac706
tipusahil <tipusahil.ctg@gmail.com>
2025-02-28 02:06:40 +0600

    settimeout & setinterval concept noted by me

--- async_and_sync._concept_in.js
+++ async_and_sync._concept_in.js
@@ -4,7 +4,7 @@
 // function secuentially cole (ortat jkn call line dhroe 5jon thakle sei function joto number serial e ase sei serail onujai se jabe, but 
 // 1.(fetch) & 
 // 2.(settimeout) & 
-// 3.(settimeinterval) ei 3ta serial ways colena, tader nijeder icce moto cole) , ortat serially cole ,
+// 3.(setInterval) ei 3ta serial ways colena, tader nijeder icce moto cole) , ortat serially cole ,
 */
 
 console.log(1);
@@ -13,11 +13,13 @@ console.log(2);
 func_secuenctiallyCole()// function secuentially cole (ortat jkn call line dhroe 5jon thakle sei function joto number serial e ase sei serail onujai se jabe, but 
 // 1.(fetch) & 
 // 2.(settimeout) & 
-// 3.(settimeinterval) ei 3ta serial ways colena, tader nijeder icce moto cole) , ortat serially cole ,
+// 3.(setInterval) ei 3ta serial ways colena, tader nijeder icce moto cole) , ortat serially cole ,
 console.log(4);
 
 setTimeout(() =>{ console.log('(setTimeout) nijer icce moto 4 second pore output dekabe,') } , 4000);
-setTimeout(() =>{ console.log('(setTimeinterval) nijer icce moto take jei time set kore dewa hoise sei time por por bar bar colte takbe.') } , 2000);
+
+setInterval(() =>{ console.log('(setInterval) nijer icce moto take jei time set kore dewa hoise sei time por por bar bar colte takbe.') } , 2000);
+
 
 console.log(5);
 
