2380d7863ce5285c25259adadd525453a6231d3f
tipusahil <tipusahil.ctg@gmail.com>
2025-02-28 17:09:23 +0600

    settimeout & setinterval concept noted by me

--- event_loop.js
+++ event_loop.js
@@ -3,7 +3,7 @@
 */
 
 function one() {
-    console.log('a');
+    console.log('a ekdm 1st ta');
     two()
     console.log('one theke aa')
 }
@@ -15,8 +15,9 @@ console.log('bb two theke ')
 }
 
 function tree() {
-    console.log('d ');
-    console.log('dd');
+    console.log('d from tree theke');
+    console.log('ddd from tree theke');
+  
 
 }
 
