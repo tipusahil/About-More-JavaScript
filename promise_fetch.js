9c1b9b66b31357a8fb21466ba8596f1a5d18e722
tipusahil <tipusahil.ctg@gmail.com>
2025-02-28 11:49:36 +0600

    settimeout & setinterval concept noted by me

---
+++ promise_fetch.js
@@ -0,0 +1,5 @@
+/* 
+1. (fetch) function ta ek dhoronoer promise er moto kaj kore, hoi api theke data dibe,nahoi errror dibe.
+(fetch) jeheto browser e cole tai kuno html file er sate linkup kore then fetch ta use korte hbe. nahoi kaj hobena.
+
+*/
