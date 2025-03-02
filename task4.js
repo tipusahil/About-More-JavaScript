7708a6d6a62270f5d63fada18bf5ad6ae0e26f85
tipusahil <tipusahil.ctg@gmail.com>
2025-03-02 13:43:13 +0600

    joke task4 completed

--- task4.js
+++ task4.js
@@ -1,1 +1,37 @@
-// api url =  https://v2.jokeapi.dev/joke/Programming?type=single
+
+async function jokeBOLO(jokesID) {
+    const url = `https://v2.jokeapi.dev/joke/Programming?id=${jokesID}`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    let jokes_joke = data.setup;
+
+    const id = data.id;
+    console.log(data)
+if( jokes_joke === undefined ){
+ jokes_joke = data.joke ;
+
+}
+
+    console.log(id , jokes_joke);
+
+    seeNewJoke(id , jokes_joke)
+}
+
+
+
+const seeNewJoke = (id , joke) => {
+
+    document.getElementById('joke_box').innerHTML =`id: ${id}   <br> 
+    joke: ${joke}`;
+    
+
+  
+}
+
+document.getElementById('new_joke_btn').addEventListener('click' , () => {
+    jokeBOLO()
+});
+
+
+
